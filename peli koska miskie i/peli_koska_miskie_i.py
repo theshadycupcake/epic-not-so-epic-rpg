32179c7be54d89fcb8c769d4657db01bb782a473
theshadycupcake <vil.salmensaari@gmail.com>
2021-12-07 15:41:24 +0200

    jaaha

--- peli koska miskie i/peli_koska_miskie_i.py
+++ peli koska miskie i/peli_koska_miskie_i.py
@@ -1,5 +1,7 @@
 import sys,time
 
+name = ""
+
 def vs_print(str):
     for letter in str:
         sys.stdout.write(letter)
@@ -19,10 +21,55 @@ def f_print(str):
         time.sleep(0.1)
 
 f_print("welcome thy fool to this world of suffering")
-input()
-vs_print("in this world you either die or live, like in any other world")
-input()
-vs_print("provide me your name")
+f_print(" ")
+print("")
+f_print("in this world you either die or live, like in any other world")
+f_print(" ")
+print("")
+f_print("what is your gender")
+print("")
+print("1. male")
+print("2. female")
+print("3. neither, fuck you")
+gender = input()
+if gender == "3":
+    f_print("well thats rude but i apologize for assuming")
+
+f_print(" ")
+print("")
+f_print("how old are you")
+print("")
+age = int(input)
+if age < 12:
+    f_print("arent you kinda young for this?")
+    f_print(" ")
+    print("")
+    f_print("well have fun kiddo")
+elif age < 12:
+    f_print("so old, soon youre even about to die")
+else:
+    f_print("is giving me a number so hard")
+
+f_print(" ")
+print("")
+f_print("provide me your name")
+print("")
 name = input()
-s_print(name)
-ff_print("didnt know there was anyone called like that, sounds kinda dumb if im honest")
+print("")
+if name == "no":
+    name = "sylvia"
+    f_print("well thats a dick move")
+    f_print(" ")
+    print("")
+    f_print("your name is")
+    print("")
+    vs_print(name)
+    f_print(" ")
+else:
+    f_print(name)
+    f_print("didnt know there was anyone called like that, sounds kinda dumb if im honest")
+    f_print(" ")
+    f_print("well whatever")
+f_print(" ")
+print("")
+f_print("")
